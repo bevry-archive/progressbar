7a30200c433ec29c8c284d5b5c7df596e1a3399e
Arne Schubert <arne.schubert@rewe-digital.com>
2018-12-20 16:22:27 +0100

    #7: Enhance with TypeScript Syntax and add typical files

---
+++ index.d.ts
@@ -0,0 +1,2 @@
+import ProgressBar from "./source";
+export = ProgressBar;
--- source/index.ts
+++ source/index.ts
@@ -1,185 +1,172 @@
-'use strict'
+import Domain from "domain";
+import { EventEmitter } from "events";
+import Progress from "progress";
 
-const Progress = require('progress')
+class ProgressBar extends EventEmitter {
+	public static ProgressBar = ProgressBar; // Backwards API Compat
 
-class ProgressBar extends require('events').EventEmitter {
-
-	static create (...args) {
-		return new this(...args)
+	public static create(): ProgressBar {
+		return new ProgressBar();
 	}
 
-	constructor (...args) {
-		super(...args)
+	private _tick = 0;
+	private _total = 1;
+	private _bar: Progress | null = null;
+	private _step: string | null = null;
+	private _domain: Domain.Domain | null = null;
 
-		this._tick = null
-		this._total = null
-		this._bar = null
-		this._step = null
-		this._domain = null
-
-		this.start()
+	constructor() {
+		super();
+		this.start();
 	}
 
-	start () {
-		const me = this
-		this._tick = 0
-		this._total = 1
-		try {
-			this._domain = require('domain').create()
+	public step(s: string): this {
+		if (s != null) {
+			this.setStep(s);
+		} else {
+			throw new Error("step is now just an alias for setStep to ensure consistent return value");
 		}
-		catch (err) { }
+		return this;
+	}
+	public getStep(): string | null {
+		return this._step;
+	}
+	public setStep(s: string): this {
+		if (!s) { throw new Error("no step param defined"); }
+		this._step = s;
+		this.emit("step", this._step);
+		this.setTick(0);
+		this.setTotal(1);
+		return this;
+	}
+
+	public total(t: number): this {
+		if (t != null) {
+			this.setTotal(t);
+		} else {
+			this.addTotal();
+		}
+		return this;
+	}
+	public getTotal(): number {
+		return this._total;
+	}
+	public addTotal(t: number = 1): this {
+		this._total += t;
+		this.emit("total", this._total);
+		return this;
+	}
+	public setTotal(t: number): this {
+		this._total = t || 1;  // must be truthy rather than null, otherwise: RangeError: Invalid array length
+		this.emit("total", this._total);
+		return this;
+	}
+
+	public tick(t: number | null) {
+		if (t != null) {
+			this.setTick(t);
+		} else {
+			this.addTick();
+		}
+		return this;
+	}
+	public getTick(): number {
+		return this._tick;
+	}
+	public addTick(t: number = 1): this {
+		this._tick += t;
+		this.emit("tick", this._tick);
+		return this;
+	}
+	public setTick(t: number): this {
+		this._tick = t;
+		this.emit("tick", this._tick);
+		return this;
+	}
+
+	public finish(next?: () => void) {
+		this.destroy(() => {
+			this.emit("finish");
+			if (this._domain) { this._domain.exit(); }
+			this.removeAllListeners();
+			if (next) { next(); }
+		});
+		return this;
+	}
+
+	protected destroy(next?: () => void) {
+		if (this._bar != null) {
+			if (this._domain) {
+				this._domain.run(() => {
+					if (this._bar) {
+						this._bar.terminate();
+					}
+				});
+				this._domain.run(() => {
+					this._bar = null;
+				});
+			} else {
+				this._bar.terminate();
+				this._bar = null;
+			}
+		}
+		if (next) { next(); }
+		return this;
+	}
+	protected start(): this {
+		this._tick = 0;
+		this._total = 1;
+		try {
+			this._domain = Domain.create();
+		} catch (err) { /* Just ignore */ }
 
 		// bubble domain errors
 		if (this._domain) {
-			this._domain.on('error', function (err) {
-				me.emit('error', err)
-			})
+			this._domain.on("error", (err) => {
+				this.emit("error", err);
+			});
 		}
 
 		// destroy the old progressbar and create our new one
-		this.on('step', function () {
-			me.destroy()
-			const message = `Performing ${me._step} at :current/:total :percent :bar`
-			if (me._domain) {
-				me._domain.run(me.onStep.bind(me, message))
+		this.on("step", () => {
+			this.destroy();
+			const message = `Performing ${this._step} at :current/:total :percent :bar`;
+			if (this._domain) {
+				this._domain.run(this.onStep.bind(this, message));
+			} else {
+				this.onStep(message);
 			}
-			else {
-				me.onStep()
-			}
-		})
+		});
 
 		// update our bar's total
-		this.on('total', function () {
-			if (me._bar) me._bar.total = me._total
-		})
+		this.on("total", () => {
+			if (this._bar) { this._bar.total = this._total; }
+		});
 
 		// update our bar's progress
-		this.on('tick', function () {
-			if (me._bar) me._bar.tick(me._tick - me._bar.curr)
-		})
+		this.on("tick", () => {
+			if (this._bar) { this._bar.tick(this._tick - this._bar.curr); }
+		});
 
 		// chain
-		return this
+		return this;
 	}
 
-	onStep (message) {
+	private onStep(message: string) {
 		try {
 			this._bar = new Progress(message, {
-				width: 50,
+				clear: true,
 				total: this._total,
-				clear: true
-			})
-		}
-		catch (err) {
+				width: 50,
+			});
+		} catch (err) {
 			if (this._domain) {
-				this._domain.emit('error', err)
-			}
-			else {
-				this.emit('error', err)
-			}
-		}
-	}
-
-	step (s) {
-		if (s != null) {
-			this.setStep(s)
-		}
-		else {
-			throw new Error('step is now just an alias for setStep to ensure consistent return value')
-		}
-		return this
-	}
-	getStep () {
-		return this._step
-	}
-	setStep (s) {
-		if (!s) throw new Error('no step param defined')
-		this._step = s
-		this.emit('step', this._step)
-		this.setTick(0)
-		this.setTotal(1)
-		return this
-	}
-
-	total (t) {
-		if (t != null) {
-			this.setTotal(t)
-		}
-		else {
-			this.addTotal()
-		}
-		return this
-	}
-	getTotal () {
-		return this._total
-	}
-	addTotal (t = 1) {
-		this._total += t
-		this.emit('total', this._total)
-		return this
-	}
-	setTotal (t) {
-		this._total = t || 1  // must be truthy rather than null, otherwise: RangeError: Invalid array length
-		this.emit('total', this._total)
-		return this
-	}
-
-	tick (t) {
-		if (t != null) {
-			this.setTick(t)
-		}
-		else {
-			this.addTick()
-		}
-		return this
-	}
-	getTick () {
-		return this._tick
-	}
-	addTick (t = 1) {
-		this._tick += t
-		this.emit('tick', this._tick)
-		return this
-	}
-	setTick (t) {
-		this._tick = t
-		this.emit('tick', this._tick)
-		return this
-	}
-
-	destroy (next) {
-		if (this._bar != null) {
-			const me = this
-			if (this._domain) {
-				this._domain.run(function () {
-					me._bar.terminate()
-				})
-				this._domain.run(function () {
-					me._bar = null
-				})
-			}
-			else {
-				me._bar.terminate()
-				me._bar = null
+				this._domain.emit("error", err);
+			} else {
+				this.emit("error", err);
 			}
 		}
-		if (next) next()
-		return this
-	}
-	finish (next) {
-		const me = this
-		this.destroy(function () {
-			me.emit('finish')
-			if (me._domain) me._domain.exit()
-			me.removeAllListeners()
-			if (next) next()
-		})
-		return this
 	}
 }
 
-// Export
-module.exports = ProgressBar
-
-// Backwards API Compat
-module.exports.ProgressBar = ProgressBar
+export = ProgressBar;
