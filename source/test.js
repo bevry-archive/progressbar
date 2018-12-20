f2ee1fa6afc50f6fb8904973993f446d52929857
Arne Schubert <arne.schubert@rewe-digital.com>
2018-12-20 14:05:11 +0100

    #7: Just switch to TypeScript

--- source/test.js
+++ source/test.js
@@ -1,20 +1,23 @@
-'use strict'
-
-const progress = require('../').create().step('the task you are currently performing')
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const progress_1 = __importDefault(require("progress"));
+const progress = progress_1.default.create().step('the task you are currently performing');
 // use an array of steps that execute one second after each other
 // as if we do them all instantly
 // you won't see the progress bar as it will be instant
 const steps = [
-	() => progress.setTotal(5),
-	() => progress.setTick(1),
-	() => progress.setTick(2),
-	() => progress.setTick(3),
-	() => progress.addTick(),
-	() => progress.addTick(),
-	() => progress.finish()  // remove and destroy the progress bar
-]
-
+    () => progress.setTotal(5),
+    () => progress.setTick(1),
+    () => progress.setTick(2),
+    () => progress.setTick(3),
+    () => progress.addTick(),
+    () => progress.addTick(),
+    () => progress.finish() // remove and destroy the progress bar
+];
 steps.forEach(function (step, index) {
-	setTimeout(step, index * 1000)
-})
+    setTimeout(step, index * 1000);
+});
+//# sourceMappingURL=test.js.map
---
+++ source/test.ts
@@ -0,0 +1,20 @@
+'use strict'
+
+const progress = require('../').create().step('the task you are currently performing')
+
+// use an array of steps that execute one second after each other
+// as if we do them all instantly
+// you won't see the progress bar as it will be instant
+const steps = [
+	() => progress.setTotal(5),
+	() => progress.setTick(1),
+	() => progress.setTick(2),
+	() => progress.setTick(3),
+	() => progress.addTick(),
+	() => progress.addTick(),
+	() => progress.finish()  // remove and destroy the progress bar
+]
+
+steps.forEach(function (step, index) {
+	setTimeout(step, index * 1000)
+})
